e0ae61de78f9a2cb528cb0ba3f76e918bedfcd58
deborapatrao <deborapcorreia@gmail.com>
2023-02-22 17:58:13 -0800

    Add query and mutations schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,4 +1,5 @@
 import { gql } from 'apollo-server-express'
+import { filter, find, remove } from 'lodash'
 
 const people = [
     {
@@ -97,27 +98,120 @@ const people = [
   const typeDefs = gql`
     type Person{
       id: String!
-      firstName: String
-      lastName: String
+      firstName: String!
+      lastName: String!
     }
 
     type Car {
       id: String!
-      year: Int
-      make: String
-      model: String
-      price: Float
-      personId: String
+      year: Int!
+      make: String!
+      model: String!
+      price: Float!
+      personId: String!
     }
 
     type Query {
-      person: [Person]
+      people: [Person]
+      cars: [Car]
+      car(personId: String!): [Car]
+    }
+
+    type Mutation{
+      addPerson(id: String!, firstName: String!, lastname:String!): Person
+      addCar(id: String!, year: Int!, make: String!, model: String!, price: Float!, personId: String!): Car
+
+      updatePerson(id:String!, firstName: String, lastName: String): Person
+      updateCar(id: String!, year: Int, make: String, model: String, price: Float, personId: String): Car
+
+      removePerson(id:String!): Person
+      removeCar(id:String!): Car
     }
   `
 
   const resolvers = {
     Query: {
-      person: () => people
+      people: () => people,
+      cars: () => cars,
+      car: (root, args) => {
+        //filter will get all the results, as opposed by find that will get only the first result
+        return filter(cars, {personId: args.personId})
+      }
+    },
+    Mutation: {
+      addPerson: (root, args) => {
+        const newPerson = {
+          id: args.id,
+          firstName: args.firstName,
+          lastName: args.lastName
+        }
+
+        people.push(newPerson)
+        return newPerson
+      },
+
+      addCar: (root, args) => {
+        const newCar = {
+          id: args.id,
+          year: args.year,
+          make: args.make,
+          model: args.model,
+          price: args.price,
+          personId: args.personId
+        }
+
+        cars.push(newCar)
+        return newCar
+      },
+
+      updatePerson: (root, args) => {
+        const person = find(people, { id: args.id })
+
+        if(!person) throw new Error(`Couldn't find person with id ${args.is}`)
+
+        person.firstName = args.firstName
+        person.lastName = args.lastName
+
+        return person
+      },
+
+      updateCar: (root, args) => {
+        const car = find(cars, {id: args.id})
+
+        if(!car) throw new Error(`Couldn't find car with id ${args.is}`)
+
+        car.year = args.year
+        car.make = args.make
+        car.model = args.model
+        car.price = args.price
+        car.personId = args.personId
+
+        return car
+      },
+
+      removePerson: (root, args) => {
+        const removedPerson = find(people, {id: args.id})
+
+        if(!removedPerson) throw new Error(`Couldn't find person with id ${args.is}`)
+
+        remove(people, p => {
+          return p.id === removedPerson.id
+        })
+
+        return removedPerson
+      }, 
+
+      removeCar: (root, args) => {
+        const removedCar = find(cars, {id: args.id})
+
+        if(!removedCar) throw new Error(`Couldn't find car with id ${args.is}`)
+
+        remove(cars, p => {
+          return p.id === removedCar.id
+        })
+
+        return removedCar
+      }, 
     }
   }
 
